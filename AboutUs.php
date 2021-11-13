053ebc9c8a94b5adb100aa8148968c59a4ee6a33
Austin <austinmillon16@gmail.com>
2021-11-13 16:32:21 -0500

    html to php

---
+++ AboutUs.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>About Us</title>
+    <meta charset="utf-8">
+</head>
+<link rel="stylesheet" href="styleSheet.css">
+  </head>
+  <body>
+    <header>
+        <a href="HomePage.php">
+        <img class="logo" src="Image/American_Bank.png" alt="Logo of the bank">
+        </a>
+      </header>
+    <hr>
+    <br>
+    <nav >
+      <ol>
+    <div class="menudown">
+      <li><a href="#">Menu</a></li>
+      <div class="menudown-content">
+      <a href="DigitalBanking.php">Digital Banking</a>
+      <a href="DepositRate.php">Deposit Rate</a>
+      <a href="AboutUs.php">About Us</a>
+      </div>
+      </div>
+      <li><a href="Login.php">Login</a> &nbsp; </li>
+        <div class="quickdown">
+          <li><a href="#">Quick Links</a></li>
+            <div class="quickdown-content">
+              <a href="Loan.php">Apply for Loan</a>
+              <a href="Credit.php">Credit Card</a>
+        
+            </div>
+        </div>
+        <li><a href="contact.php">Contact Us</a></li> 
+      </ol>
+   
+    </nav>
+    </body><br><hr>
+    <h1>About Us</h1>
+    
+<div class="AboutUs">American Bank, a wholly-owned subsidiary of American Bank, Inc., is a state-chartered, FDIC insured, “nationwide” community bank, serving both our local community in Eastern Pennsylvania as well as our customers throughout the country from our headquarters in Allentown, Pennsylvania.<br>
+
+    Our mission is to help people spend smarter, manage debt better, and save more to achieve a brighter financial future.<br>
+
+    We are committed and delivering on our mission to help people achieve a brighter financial future.<br>
+
+    In an effort to make online banking fast, convenient, and safe, American Bank’s comprehensive online banking solution provides our customers with an easy-to-navigate, simplified environment.  Customers are able to pay bills, transfer funds, view their account register, and so much more, all in real-time! So, if you’re not an American Bank customer, experience what so many others already have … become a member of the American Bank family today!</div>
+
+    </html>
